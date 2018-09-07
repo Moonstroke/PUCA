2ffb672e7a8249ae8d669c25d04d919815249460
joH1 <jiema@live.fr>
2018-09-07 16:24:25 +0200

    Fix whitespace
    Empty lines are removed, indentation blanks are added for `define`
    directives in the non-GNU C branch.

--- funcattrs.h
+++ funcattrs.h
@@ -5,32 +5,21 @@
 
 #ifdef __GNUC__
 # define INLINE __attribute__((__always_inline__)) inline
-
 # define NODISCARD __attribute__((__warn_unused_result__))
-
 # define NORETURN __attribute__((__noreturn__))
-
 # define NOTNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
-
 # define PURE __attribute__((__pure__,__warn_unused_result__))
 # define CONSTEXPR __attribute__((__const__,__warn_unused_result__))
-
 # define MALLOC __attribute__((__malloc__,__warn_unused_result__))
 
 #else /* __GNUC__ */
-
-#define INLINE inline
-
-#define NODISCARD
-
-#define NORETURN
-
-#define NOTNULL(...)
-
-#define PURE
-#define CONSTEXPR
-
-#define MALLOC
+# define INLINE inline
+# define NODISCARD
+# define NORETURN
+# define NOTNULL(...)
+# define PURE
+# define CONSTEXPR
+# define MALLOC
 
 #endif
 
