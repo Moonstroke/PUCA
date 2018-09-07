329f271ec7c4347a47a8824dae3a7b749572a0a1
joH1 <jiema@live.fr>
2018-09-07 16:16:33 +0200

    Attributes PURE, CONST and MALLOC imply NODISCARD

--- funcattrs.h
+++ funcattrs.h
@@ -10,10 +10,10 @@
 
 # define NOTNULL(...) __attribute__((__nonnull__(__VA_ARGS__)))
 
-# define PURE __attribute__((__pure__))
-# define CONST __attribute__((__const__))
+# define PURE __attribute__((__pure__,__warn_unused_result__))
+# define CONST __attribute__((__const__,__warn_unused_result__))
 
-# define MALLOC __attribute__((__malloc__))
+# define MALLOC __attribute__((__malloc__,__warn_unused_result__))
 
 #else /* __GNUC__ */
 
