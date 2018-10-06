76fe256f259d12cf95f0f3618aacd6a1cb169295
joH1 <jiema@live.fr>
2018-10-06 22:39:52 +0200

    Document end_varattrs.h

--- end_varattrs.h
+++ end_varattrs.h
@@ -1,3 +1,14 @@
+/**
+ * \file end_varattrs.h
+ * \author joH1
+ * \version 0.1
+ *
+ * \brief Include this file to remove variable attributes.
+ *
+ * This file contains \c #undef directives to remove the definitions of the
+ * variable attributes declared in varattrs.h, to clean up the macros namespace.
+ */
+
 #ifndef VAR_ATTRS_H
 # warning end_varattrs.h included without varattrs.h
 #endif
